30d7164ebedcd9b70e2d1d153d57b3cb2763c857
TIEN ANH NGUYEN PHONG <1821050787@student.humg.edu.vn>
2021-06-06 16:14:11 +0700

    add

---
+++ NguyenPhongTienAnh_2/Migrations/202106060907484_NhaCungCap.cs
@@ -0,0 +1,16 @@
+namespace NguyenPhongTienAnh_2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NhaCungCap : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ NguyenPhongTienAnh_2/Models/SanPham.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NguyenPhongTienAnh_2.Models
+{
+    public class SanPham
+    {
+        [key]
+        public int MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public int MaNhaCungCap { get; set; }
+    }
+}
